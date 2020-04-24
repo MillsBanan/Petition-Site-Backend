6f728fce5c0a9b326bfd167c30cdfe73b428478c
Ryan <rrm36@uclive.ac.nz>
2020-04-24 14:34:53 +1200

    yeetus

--- app/controllers/petition.controller.js
+++ app/controllers/petition.controller.js
@@ -13,41 +13,43 @@ exports.list = async function(req, res) {
         "startIndex": req.query.startIndex
     };
     try {
-        checkListQueryParams(queryParams);
-    } catch {
-        res.status(400)
-            .send(`ERROR listing petitions`);
-    }
-
-    try {
-        const result = await petition.getAll(queryParams);
-        let start = 0;
-        let finish = result.length;
-        if (queryParams.startIndex !== undefined ) {
-            start = parseInt(queryParams.startIndex);
+        if (!queryParamsOk(queryParams)) {
+            res.status(400)
+                .send();
+        } else if (await petition.categoryInvalid(queryParams.categoryId)) {
+            res.status(400)
+                .send();
+        } else {
+            const result = await petition.getAll(queryParams);
+            let start = 0;
+            let finish = result.length;
+            if (queryParams.startIndex !== undefined) {
+                start = parseInt(queryParams.startIndex);
+            }
+            if (queryParams.count !== undefined) {
+                finish = parseInt(queryParams.count);
+            }
+            res.status(200)
+                .send(result.slice(start, finish + 1));
         }
-        if (queryParams.count !== undefined) {
-            finish = parseInt(queryParams.count);
-        }
-        res.status(200)
-            .send(result.slice(start, finish + 1));
     } catch(err) {
         res.status(500)
             .send(`ERROR listing petitions: ${err}`);
     }
 };
 
-function checkListQueryParams(queryParams) {
-
+function queryParamsOk(queryParams) {
     if (queryParams.categoryId !== undefined && !(Number.isInteger(parseFloat(queryParams.categoryId)))) {
-        throw "Value given for categoryId is not an integer!";
-
+        return false;
     } else if (queryParams.authorId !== undefined && !(Number.isInteger(parseFloat(queryParams.authorId)))) {
-        throw "Value given for authorId is not an integer!";
-
+        return false;
     } else if (queryParams.sortBy !== undefined && !(["SIGNATURES_DESC", "SIGNATURES_ASC",
                 "ALPHABETICAL_DESC", "ALPHABETICAL_ASC"].includes(queryParams.sortBy))) {
-        throw "Value given to sort by was invalid!";
+        return false;
+    } else if (queryParams.startIndex !== undefined && queryParams.startIndex < 0) {
+        return false;
+    } else {
+        return true;
     }
 }
 
@@ -63,7 +65,7 @@ exports.create = async function(req, res) {
                 petitionData.categoryId === undefined) {
             res.status(400)
                 .send();
-        } else if (Date.now() > Date.parse(petitionData.closingDate)) {
+        } else if (Date.now() >= Date.parse(petitionData.closingDate)) {
             res.status(400)
                 .send();
         } else if (await petition.categoryInvalid(petitionData.categoryId)) {
